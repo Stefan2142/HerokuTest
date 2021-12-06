9c762f26516f87409dcdb29b02520861cad9fb0e
Stefan2142 <stefan_2142@outlook.com>
2021-12-07 00:32:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
-
+from whitenoise import WhiteNoise
 
 from apps import dashboard, routines
 
